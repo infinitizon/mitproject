251e7d06a418aef5ca98b142dd53d9bb9a3e0b4f
Abimbola Hassan <ahassan@Karachi.local>
2019-12-12 11:56:58 +0100

    Completed the Python sandbox

--- PythonSandbox/index.py
+++ PythonSandbox/index.py
@@ -1,30 +1,49 @@
-# import json
 from flask import Flask,request,jsonify
 from sandbox import Sandbox
+from functools import partial
+import time
+import os
+
+output_buffer = None
+print_orig = print
+
 app = Flask(__name__)
- 
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
+
+def ob_start(fname):
+    global print
+    global output_buffer
+    output_buffer = open(fname, 'w')
+    print = partial(print_orig, file=output_buffer)
+def ob_end():
+    global output_buffer
+    output_buffer.close()
+    print = print_orig
+def ob_get_contents(fname):
+    global output_buffer
+    data = open(fname, 'r').read()
+    if os.path.exists(fname):
+        os.remove(fname)
+    return data
+
 @app.route("/", methods=['POST'])
 def hello():
     return "Welcome to Python Flask!"
 
 @app.route("/sandbox/", methods=['POST'])
 def sandbox():
-    # editorContent =  request.form['myEditor']
-    # content = request.form['myEditor']
-    # print(content)
-    # return jsonify(content)
-    # print(jsonify({'status':'OK','user':request.form['myEditor']}))
-    # return "editorContent"
-    # response = app.response_class(
-    #     response=json.dumps(request.form['myEditor']),
-    #     status=200,
-    #     mimetype='application/json'
-    # )
-    # return response
-    s = Sandbox()
-    code = request.form['myEditor']
-    return s.execute(code)
-    # return ""
-    
+    now = str(int(round(time.time() * 1000))) + '.txt'  # Create a random file on the fly
+    code = request.form['myEditor']                     # Get the content from the post field
+    ob_start(now)                                       # Start the output buffer
+    exec(code)
+    ob_end()
+    return ob_get_contents(now)
+
 if __name__ == "__main__":
     app.run()
--- PythonSandbox/sandbox.py
+++ PythonSandbox/sandbox.py
@@ -1,6 +1,6 @@
 class Sandbox(object):
     def execute(self, code_string):
-        return exec(code_string)
+        exec(code_string)
         # try:
         #     return eval(code_string)
         # except Exception as e:
