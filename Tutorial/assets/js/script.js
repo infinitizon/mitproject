ac757916c3f1942030c27a9f7ee309c49ffad0b8
Abimbola Hassan <ahassan@Karachi.local>
2020-01-27 17:23:37 +0100

    Working on the HTML

--- Tutorial/assets/js/script.js
+++ Tutorial/assets/js/script.js
@@ -5,10 +5,6 @@ $(function(){
         $node = $(this).attr("data-node");
         $content = $(this).attr("data-content");
 
-        console.log($lang);
-        console.log($node);
-        console.log($content);
-        console.log(config[$lang][$node]+$content);
         $("#frame").attr("src", config[$lang][$node]+$content);
     });
 });
