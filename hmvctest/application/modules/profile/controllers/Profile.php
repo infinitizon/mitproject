19cbe4cb6debab2bfee6369228cc139614c75312
Abimbola <infinitizon@gmail.com>
2020-03-13 21:18:34 +0100

    Worked on the profile page

--- hmvctest/application/modules/profile/controllers/Profile.php
+++ hmvctest/application/modules/profile/controllers/Profile.php
@@ -4,11 +4,62 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Profile extends MX_Controller {
 	public function index()
 	{
+		$this->load->model('Common'); $this->Common->check_login();
 		$data['module'] = "profile";
 		$data['view_file'] = "index"; 
-		
-		$this->load->model('test/tests');
 		$data['pageTitle'] = "My Profile"; 
-		echo Modules::run("templates/general", $data);
+
+		$data['styles'] = [
+			assets_url()."/plugins/sweetalert/css/sweetalert.css",
+		];
+		$data['scripts'] = [
+			assets_url()."/plugins/sweetalert/js/sweetalert.min.js",
+			assets_url()."/js/admin/profile.js",
+		];
+
+		// First get currently logged in user
+		$this->db->select("u.*")
+			->from('users u')
+			->where('u.r_k', $this->session->userdata('logged_in')->r_k);
+		$data['user'] = $this->db->get()->result()[0];
+		
+		if($this->input->post()){
+			$this->load->library('form_validation');
+			if($this->input->post('email') != '')
+				$this->form_validation->set_rules('email','Email','valid_email|trim');
+			if($this->input->post('cpass')) {
+				if(md5($this->input->post('cpass')) != $data['user']->password){
+					$fields = array('cpass' => 'Current password entered is incorrect');
+					$result = array('success'=>false,'message'=>$fields);
+					echo json_encode($result); exit;
+				} elseif ($this->input->post('npass') != $this->input->post('npassAgain')) {
+					$fields = array('npass' => 'New passwords entered do not match');
+					$result = array('success'=>false,'message'=>$fields);
+					echo json_encode($result); exit;
+				}
+			} 
+			
+			if($this->form_validation->run()) {
+				// var_dump($this->input->post());
+				
+				$fields = array(
+					'r_k' => $this->session->userdata('logged_in')->r_k,
+					'email' => $this->input->post('email'),
+					'password' => md5($this->input->post('npass')),);
+				$this->Common->setTable('users');
+				$this->Common->_insert_on_duplicate_update($fields) ;
+				$result = array('success'=>true,'message'=>'Profile updated successfully', 'fields'=>$_POST);
+			}else{
+				$fields = array(
+					'email' => form_error('email'),
+					'cpass' => form_error('cpass'),
+					'npass' => form_error('npass'),
+					'npassAgain' => form_error('npassAgain'),);
+				$result = array('success'=>false,'message'=>$fields);
+			}
+			echo json_encode($result);
+		} else {
+			echo Modules::run("templates/admin", $data);
+		}
 	}
 }
--- hmvctest/application/modules/profile/views/index.php
+++ hmvctest/application/modules/profile/views/index.php
@@ -1,4 +1,45 @@
-<?php
-// echo $this->router->class;
-// echo $this->router->method;
-// echo $content->message->content ;
+<div class="col-lg-12">
+    <div class="card">
+        <div class="card-body">
+            <h4 class="card-title">Vertical Form</h4>
+            <div class="row">
+                <div class="alert alert-dismissible hide fade in ">
+                    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                    <strong></strong><span class="message"></span>
+                </div>
+            </div>
+            <div class="basic-form">
+                <form id="profile" action="<?php echo base_url().'profile' ?>" method="post">
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <label>Email</label>
+                            <input type="email" class="form-control" name="email" id="email" placeholder="Email" value="<?php echo $user->email; ?>">
+                            <span class="error text-danger"></span>
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <label>Current Password</label>
+                            <div><small>To change your password, enter you old password, then your preferred password</small></div>
+                            <input type="password" class="form-control" name="cpass" id="cpass" placeholder="Enter Current Password">
+                            <span class="error text-danger"></span>
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <label>Preferred Password</label>
+                            <input type="password" class="form-control" name="npass" id="npass" placeholder="Enter Preferred Password">
+                            <span class="error text-danger"></span>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label>Preferred Password Again</label>
+                            <input type="password" class="form-control" name="npassAgain" id="npassAgain" placeholder="Enter Preferred Password Again">
+                            <span class="error text-danger"></span>
+                        </div>
+                    </div>
+                    <button type="submit" class="btn btn-dark">Update</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
